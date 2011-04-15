f87f9fd120c771858ce10c759467f27a2c6e863d
otteroe <otteroe@gmail.com>
2011-04-15 11:38:36 +0200

    Ser om det här fungerar med int till byte i java.

--- testklient1.java
+++ testklient1.java
@@ -5,24 +5,30 @@ import java.util.Scanner;
 public class testklient1{
 
   Socket client;
-  PrintWriter out;
+  OutputStream out;
   BufferedReader in;
   Scanner sc;
   public String name;
 
-  private void sendStr(String msg){
+/*  private void sendStr(String msg){
       out.write(msg);
       out.flush();
   }
+*/
 
   private void sendInt(int msg){
 	byte[] newMsg = new byte[4];
-	newMsg[0] = (byte)(msg >> 24);
+	newMsg[0] = (byte)(msg >>> 24);
 	newMsg[1] = (byte)((msg << 8) >> 24);
 	newMsg[2] = (byte)((msg << 16) >> 24);
 	newMsg[3] = (byte)((msg << 24) >> 24);
+	try{
     out.write(newMsg);
     out.flush();
+	}
+	catch(IOException e){
+	client.close();
+	}
   }
 
   public static void main(String[] args){
@@ -31,7 +37,7 @@ public class testklient1{
       tk.sc = new Scanner(System.in);
       tk.client = new Socket("localhost", 1233);
       System.out.println("connected");
-      tk.out = new PrintWriter(tk.client.getOutputStream());
+      tk.out = tk.client.getOutputStream();
       tk.in = new BufferedReader(new InputStreamReader(tk.client.getInputStream()));
     } 
     catch (IOException ioe) { ioe.printStackTrace(); }
