567ba31d86e5601180611eefdfc12b69ccc92c4b
Magnus Eriksson <Chylis88gmail.com>
2011-04-15 17:38:50 +0200

    fixade så att servern svarar klienten med dem korrekta bitvärdena, signed etc

--- testserver1.erl
+++ testserver1.erl
@@ -2,48 +2,78 @@
 
 -export([init/0, accept/2]).
 
+-define(INTEGER, :32/signed-big-integer).
+-define(BOOLEAN, :8/signed-big-integer).
+-define(HEADER, :8/signed-big-integer).
+
 init() ->
 	{ok, ListenSocket} = gen_tcp:listen(1234, [binary, {active, false}]),
 	accept(ListenSocket, 0).
 
 accept(ListenSocket, Count) ->
 	{ok, Socket} = gen_tcp:accept(ListenSocket),
-	io:format("Accepted\n"),
+	io:format("Accepted Connection\n"),
 	spawn(?MODULE, accept, [ListenSocket, Count+1]),
 	recv(Socket).
 
-decodeHeader(<<Header:8, Rest/binary>>) -> %unsigned, TODO: FIXA SIGNED!
-	io:format("~w\n", [Header]),
-	io:format("~w\n", [Rest]),
-	{Header, Rest}.
+decodeHeader(<<Head?HEADER, RestOfPacket/binary>>) -> %Tar ut den första byten - signed, TODO: FIXA SIGNED!
+	{Head, RestOfPacket}.
 
-decodeInteger(<<Int:32, Rest/binary>>) ->
-	{Int, Rest}.
+decodeInteger(<<Int?INTEGER, RestOfPacket/binary>>) -> %Tar ut de första 4 bytes och tolkar som Integer. - FIXA SIGNED!
+	{Int, RestOfPacket}.
 
 fail(FailureType) ->
 	case FailureType of
+
+		-1 ->
+			Msg = "Invalid state";
 		0 ->
-			Msg = "Invalid Protocol Version"
-	end,
-	Msg.
+			Msg = "Invalid protocol version";
+		1 ->
+			Msg = "Name already exists";
+		2 ->
+			Msg = "Game does not exist";
+		3 ->
+			Msg = "Game is locked";
+		4 ->
+			Msg = "Permission denied";
+		5 ->
+			Msg = "Occupied tile";
+		6 ->
+			Msg = "No movement left";
+		7 ->
+			Msg = "Invalid tile";
+		8 ->
+			Msg = "Build in process";
+		9 ->
+			Msg = "Building already exists";
+		10 ->
+			Msg = "Friendly fire is off";
+		11 ->
+			Msg = "Out of range";
+		12 ->
+			Msg = "Building does not exist";
+end,
+Msg.
 
 recv(Socket) ->
 	case gen_tcp:recv(Socket, 0) of
-		{ok, Packet} -> %Data = Binärt
-			io:format("tog emot ok, Data\n"),
+		{ok, Packet} -> 
 			{Header, RestOfMsg} = decodeHeader(Packet),
 			case Header of 
 				2 ->
 					{Int, RestOfMsg2} = decodeInteger(RestOfMsg),
-					case Int =:= 0 of
+					case Int =:= -1 of
 						true ->
-							Msg = list_to_binary([term_to_binary(3)]),
+							io:format("-1"),
+							Msg = list_to_binary([3]),
 							gen_tcp:send(Socket, Msg);
 						false ->
-							io:format("false"),
-							FailMsg = list_to_binary([term_to_binary(0), term_to_binary(0), term_to_binary(fail(0))]), 
-							gen_tcp:send(Socket, FailMsg),
-							io:format("skickat")
+							B1 = <<0:8>>,
+							B2 = <<0:32>>,
+							FailMsg = list_to_binary([B1, B2, fail(0)]), 
+							io:format("~w\n", [FailMsg]),
+							gen_tcp:send(Socket, FailMsg)
 					end
 			end,
 			recv(Socket);
