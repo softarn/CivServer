0b248d4265a09bca46ff7982cd3d10e518659294
Magnus Eriksson <Chylis88gmail.com>
2011-04-15 15:17:32 +0200

    fungerar med header och integer, tolkas dock ej som signed

--- testklient1.java
+++ testklient1.java
@@ -8,41 +8,39 @@ public class testklient1{
   OutputStream out;
   BufferedReader in;
   Scanner sc;
-  public String name;
 
-/*  private void sendStr(String msg){
+  /*  private void sendStr(String msg){
       out.write(msg);
       out.flush();
   }
-*/
+  */
 
-  private void sendInt(int msg){
-	byte[] newMsg = new byte[4];
-	newMsg[0] = (byte)(msg >>> 24);
-	newMsg[1] = (byte)((msg << 8) >> 24);
-	newMsg[2] = (byte)((msg << 16) >> 24);
-	newMsg[3] = (byte)((msg << 24) >> 24);
-	try{
-    out.write(newMsg);
-    out.flush();
-	}
-	catch(IOException e){
-		System.out.println("Något gick fel när det skulle skickas.");
-	}
+  private void sendInt(int header, int y){
+    byte[] newMsg = new byte[5];
+    newMsg[0] = (byte)(header);
+    newMsg[1] = (byte)(y>>>24);
+    newMsg[2] = (byte)((y<<8)>>>24);
+    newMsg[3] = (byte)((y<<16)>>>24);
+    newMsg[4] = (byte)((y<<24)>>>24);
+    try{
+      out.write(newMsg);
+      out.flush();
+    }
+    catch(IOException e){
+      System.out.println("Något gick fel när det skulle skickas.");
+    }
   }
 
   public static void main(String[] args){
-      testklient1 tk = new testklient1();
+    testklient1 tk = new testklient1();
     try{
       tk.sc = new Scanner(System.in);
-      tk.client = new Socket("localhost", 1233);
+      tk.client = new Socket("localhost", 1234);
       System.out.println("connected");
       tk.out = tk.client.getOutputStream();
       tk.in = new BufferedReader(new InputStreamReader(tk.client.getInputStream()));
     } 
     catch (IOException ioe) { ioe.printStackTrace(); }
-    System.out.println("Name: ");
-    tk.name = tk.sc.nextLine();
     while (true){
       System.out.println("Msg att skicka: ");
       tk.sendInt(tk.sc.nextInt());
