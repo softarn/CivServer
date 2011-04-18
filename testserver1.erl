2e4702b14b9326ad79da6c85eee1bb70cbb2d02f
Magnus Eriksson <Chylis88gmail.com>
2011-04-18 11:29:49 +0200

    skapade mer generell 'createFailPackage' metod

--- testserver1.erl
+++ testserver1.erl
@@ -5,6 +5,7 @@
 -define(INTEGER, :32/signed-big-integer).
 -define(BOOLEAN, :8/signed-big-integer).
 -define(HEADER, :8/signed-big-integer).
+-define(PROTOCOLVERSION, 0).
 
 init() ->
 	{ok, ListenSocket} = gen_tcp:listen(1234, [binary, {active, false}]),
@@ -16,64 +17,74 @@ accept(ListenSocket, Count) ->
 	spawn(?MODULE, accept, [ListenSocket, Count+1]),
 	recv(Socket).
 
-decodeHeader(<<Head?HEADER, RestOfPacket/binary>>) -> %Tar ut den första byten - signed, TODO: FIXA SIGNED!
+decodeHeader(<<Head?HEADER, RestOfPacket/binary>>) -> % Tar ut den första byten och tolkar som Headern utifrån protokollet. SKA VARA BINARY ELLER BITS/BITSTRING? BINARY TROR JAG MÅSTE VARA DELBART MED 8
 	{Head, RestOfPacket}.
 
-decodeInteger(<<Int?INTEGER, RestOfPacket/binary>>) -> %Tar ut de första 4 bytes och tolkar som Integer. - FIXA SIGNED!
+decodeInteger(<<Int?INTEGER, RestOfPacket/binary>>) -> % Tar ut de första 4 bytes och tolkar som Integer utifrån protokollet.
 	{Int, RestOfPacket}.
 
-fail(FailureType) ->
+getFailMsg(FailureType) ->
 	case FailureType of
-
 		-1 ->
-			Msg = "Invalid state";
+			Msg = "Invalid state\0";
 		0 ->
-			Msg = "Invalid protocol version";
+			Msg = "Invalid protocol version\0";
 		1 ->
-			Msg = "Name already exists";
+			Msg = "Name already exists\0";
 		2 ->
-			Msg = "Game does not exist";
+			Msg = "Game does not exist\0";
 		3 ->
-			Msg = "Game is locked";
+			Msg = "Game is locked\0";
 		4 ->
-			Msg = "Permission denied";
+			Msg = "Permission denied\0";
 		5 ->
-			Msg = "Occupied tile";
+			Msg = "Occupied tile\0";
 		6 ->
-			Msg = "No movement left";
+			Msg = "No movement left\0";
 		7 ->
-			Msg = "Invalid tile";
+			Msg = "Invalid tile\0";
 		8 ->
-			Msg = "Build in process";
+			Msg = "Build in process\0";
 		9 ->
-			Msg = "Building already exists";
+			Msg = "Building already exists\0";
 		10 ->
-			Msg = "Friendly fire is off";
+			Msg = "Friendly fire is off\0";
 		11 ->
-			Msg = "Out of range";
+			Msg = "Out of range\0";
 		12 ->
-			Msg = "Building does not exist";
+			Msg = "Building does not exist\0"
 end,
 Msg.
+
+createFailPacket(<<FailureID?INTEGER>>, <<ReqHeader?INTEGER>>, Socket) -> % 1:Number to identify failure. 2:Head of failed package. NEGATIVT FAILUREID FUNGERAR EJ MED LIST2BINARY 
+	io:format("Inne i createFailPacket\n"),
+	FailText = getFailMsg(FailureID), % Text that describes the failure
+	io:format(FailText),
+	FailMsg = list_to_binary([FailureID, ReqHeader, FailText]),
+	%io:format("~w\n", [FailMsg]),
+	gen_tcp:send(Socket, FailMsg),
+	FailMsg.
 
 recv(Socket) ->
 	case gen_tcp:recv(Socket, 0) of
 		{ok, Packet} -> 
-			{Header, RestOfMsg} = decodeHeader(Packet),
+			{Header, PacketAfterHeader} = decodeHeader(Packet),
 			case Header of 
 				2 ->
-					{Int, RestOfMsg2} = decodeInteger(RestOfMsg),
-					case Int =:= -1 of
+					{Int, RestOfMsg2} = decodeInteger(PacketAfterHeader),
+					io:format("Int = "), 
+					io:format("~w\n", [Int]),
+					case Int =:= ?PROTOCOLVERSION of
 						true ->
-							io:format("-1"),
 							Msg = list_to_binary([3]),
 							gen_tcp:send(Socket, Msg);
 						false ->
-							B1 = <<0:8>>,
-							B2 = <<0:32>>,
-							FailMsg = list_to_binary([B1, B2, fail(0)]), 
-							io:format("~w\n", [FailMsg]),
-							gen_tcp:send(Socket, FailMsg)
+							createFailPacket(<<0?INTEGER>>, <<Header?INTEGER>>, Socket)
+							%B1 = <<0:8>>,
+							%B2 = <<0:32>>,
+							%FailMsg = list_to_binary([B1, B2, getFailMsg(0)]), 
+							%io:format("~w\n", [FailMsg]),
+							%gen_tcp:send(Socket, FailMsg)
 					end
 			end,
 			recv(Socket);
