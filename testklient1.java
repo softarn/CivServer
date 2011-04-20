debc2c10d92c7974dea88739eabb604a8a5a11e8
Magnus Eriksson <Chylis88gmail.com>
2011-04-20 12:05:59 +0200

    fixade simons barrocka indentering

--- testklient1.java
+++ testklient1.java
@@ -4,247 +4,247 @@ import java.util.ArrayList;
 
 public class testklient1
 {
-	private Socket 		m_clientSocket;
-	private OutputStream 	m_outStream;
-	private InputStream	m_inStream;
-	private boolean		turn = false;
-	private final int	protocolVersion = 1;
-	
-	public testklient1(String host, int port)
-	{
-		try
-		{
-			m_clientSocket 	= new Socket(host, port);
-			m_outStream 	= m_clientSocket.getOutputStream();
-			m_inStream	= m_clientSocket.getInputStream();
-			
-			System.out.println("[+] Connected to server \""+ host +"\" on port "+ port);
-		}
-		catch(UnknownHostException e)
-		{
-			e.printStackTrace();
-		}
-		catch(IOException e)
-		{
-			e.printStackTrace();
-		}
-	}
+    private Socket 		m_clientSocket;
+    private OutputStream 	m_outStream;
+    private InputStream	m_inStream;
+    private boolean		turn = false;
+    private final int	protocolVersion = 1;
 
-	// Sends the packet.
-	private void send(Packet p)
-	{
-		try
-		{
-			m_outStream.write(p.getBuffer());
-			m_outStream.flush();
-		} 
-		catch (IOException ioe)
-		{
-			ioe.printStackTrace();
-		}
-	}
+    public testklient1(String host, int port)
+    {
+        try
+        {
+            m_clientSocket 	= new Socket(host, port);
+            m_outStream 	= m_clientSocket.getOutputStream();
+            m_inStream	= m_clientSocket.getInputStream();
 
-	private String receive()		// Waits for an answer from the server and parses what it gets.
-	{
-		try
-		{
-			int header = m_inStream.read();		// First the header.
+            System.out.println("[+] Connected to server \""+ host +"\" on port "+ port);
+        }
+        catch(UnknownHostException e)
+        {
+            e.printStackTrace();
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
 
-			// Header 3, welcome to the real world.
-			if(header == 3)
-			{
-				return "Welcome to the Real World";
-			}
+    // Sends the packet.
+    private void send(Packet p)
+    {
+        try
+        {
+            m_outStream.write(p.getBuffer());
+            m_outStream.flush();
+        } 
+        catch (IOException ioe)
+        {
+            ioe.printStackTrace();
+        }
+    }
 
-			// Header 0, fail'd.
-			else if(header == 0)
-			{
-				int theFail = receiveInt();
-				int reqFail = m_inStream.read();
-				System.out.println("Fail'd\nWhat fail: " + theFail + "\nRequest that fail'd: " + reqFail + "\n" + receiveString());
-				return "Fail'd";
-			}
+    private String receive()		// Waits for an answer from the server and parses what it gets.
+    {
+        try
+        {
+            int header = m_inStream.read();		// First the header.
 
-			// Header 1, the confirm'd, but right now we receive a string for testing purposes.
-			else if(header == 1)
-			{
-				return receiveInt() + " " + receiveString();
-			}
+            // Header 3, welcome to the real world.
+            if(header == 3)
+            {
+                return "Welcome to the Real World";
+            }
 
-			// Header 6, List-testing but listGameAnswer later.
-			else if(header == 6)
-			{
-				ArrayList toReturn = null;
-				int size = receiveInt();
-				String type = receiveString();
-				if(type.equals("String"))
-				{
-					toReturn = receiveListString(size);
-				}
-				else if(type.equals("Integer"))
-				{
-					toReturn = receiveListInteger(size);
-				}
-				return toReturn.toString();
-			}
-		}catch(IOException e)
-		{
-			e.printStackTrace();
-		}	
-		return "Fail'd";
-	}
+            // Header 0, fail'd.
+            else if(header == 0)
+            {
+                int theFail = receiveInt();
+                int reqFail = m_inStream.read();
+                System.out.println("Fail'd\nWhat fail: " + theFail + "\nRequest that fail'd: " + reqFail + "\n" + receiveString());
+                return "Fail'd";
+            }
 
-	// Läser in ett heltal från inStream:en.
-	private int receiveInt()
-	{
-		byte[] toParse = new byte[4];
-		try
-		{
-			m_inStream.read(toParse);
-		}catch(IOException e)
-		{
-			e.printStackTrace();
-		}
-		int toReturn = ((int)toParse[0]<<24)
-				+(makeInt(toParse[1])<<16)
-				+(makeInt(toParse[2])<<8)
-				+(makeInt(toParse[3]));
-		return toReturn;
-	}
+            // Header 1, the confirm'd, but right now we receive a string for testing purposes.
+            else if(header == 1)
+            {
+                return receiveInt() + " " + receiveString();
+            }
 
-	// Gör om 4 bytes till ett heltal.
-	private int makeInt(byte b)
-	{
-		if((int)b < 0)
-		{
-			return ((int)b)+256;
-		}
-		else
-		{
-			return (int)b;
-		}
-	}
-	
-	// To receive Strings from the server.
-	private String receiveString() 
-	{
-		String toReturn = "";
-		for(;;)
-		{
-			try
-			{
-				int chara = m_inStream.read();
-				if(chara == -1)
-				{
-					break;
-				}
-				else if(chara == 0)
-				{
-					break;
-				}
-				else
-				{
-					toReturn+=(char)chara;
-				}
-			}catch(IOException e)
-			{
-				e.printStackTrace();
-			}
-		}
-		return toReturn;
-	}
+            // Header 6, List-testing but listGameAnswer later.
+            else if(header == 6)
+            {
+                ArrayList toReturn = null;
+                int size = receiveInt();
+                String type = receiveString();
+                if(type.equals("String"))
+                {
+                    toReturn = receiveListString(size);
+                }
+                else if(type.equals("Integer"))
+                {
+                    toReturn = receiveListInteger(size);
+                }
+                return toReturn.toString();
+            }
+        }catch(IOException e)
+        {
+            e.printStackTrace();
+        }	
+        return "Fail'd";
+    }
 
-	// Läser in en lista och returnerar den.
-	private ArrayList<Integer> receiveListInteger(int size)
-	{
-		ArrayList<Integer> toReturn = new ArrayList<Integer>();
-		for(int i=0; i<size; i++)
-		{
-		/*	try
-			{
-				toReturn.add((int)m_inStream.read());
-			}
-			catch(IOException e)
-			{
-				e.printStackTrace();
-			}
-		*/
-			toReturn.add(receiveInt());
-		}
-		return toReturn;
-	}
+    // Läser in ett heltal från inStream:en.
+    private int receiveInt()
+    {
+        byte[] toParse = new byte[4];
+        try
+        {
+            m_inStream.read(toParse);
+        }catch(IOException e)
+        {
+            e.printStackTrace();
+        }
+        int toReturn = ((int)toParse[0]<<24)
+            +(makeInt(toParse[1])<<16)
+            +(makeInt(toParse[2])<<8)
+            +(makeInt(toParse[3]));
+        return toReturn;
+    }
 
-	// Läser in en lista och returnerar den.
-	private ArrayList<String> receiveListString(int size)
-	{
-		ArrayList<String> toReturn = new ArrayList<String>();
-		for(int i=0; i<size; i++)
-		{
-			toReturn.add(receiveString());
-		}
-		return toReturn;
-	}
+    // Gör om 4 bytes till ett heltal.
+    private int makeInt(byte b)
+    {
+        if((int)b < 0)
+        {
+            return ((int)b)+256;
+        }
+        else
+        {
+            return (int)b;
+        }
+    }
 
-	// Public method for connecting to the server.
-	public String connect(String name)
-	{
-		Packet toSend = new Packet((byte)2);
-		toSend.add(protocolVersion);
-                toSend.add(name);
-		send(toSend);
-		return receive();
-	}
-	
-	// Method to test sending and receiving lists.
-	public String listTest(ArrayList list)
-	{
-		Packet toSend = new Packet((byte)13);
-		toSend.add(list);
-		send(toSend);
-		return receive();
-	}
+    // To receive Strings from the server.
+    private String receiveString() 
+    {
+        String toReturn = "";
+        for(;;)
+        {
+            try
+            {
+                int chara = m_inStream.read();
+                if(chara == -1)
+                {
+                    break;
+                }
+                else if(chara == 0)
+                {
+                    break;
+                }
+                else
+                {
+                    toReturn+=(char)chara;
+                }
+            }catch(IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+        return toReturn;
+    }
 
-	// Method for testing-purposes
-	public String serverTest()
-	{
-		send(new Packet((byte)1));
-		return receive();
-	}
+    // Läser in en lista och returnerar den.
+    private ArrayList<Integer> receiveListInteger(int size)
+    {
+        ArrayList<Integer> toReturn = new ArrayList<Integer>();
+        for(int i=0; i<size; i++)
+        {
+            /*	try
+                {
+                toReturn.add((int)m_inStream.read());
+                }
+                catch(IOException e)
+                {
+                e.printStackTrace();
+                }
+                */
+            toReturn.add(receiveInt());
+        }
+        return toReturn;
+    }
 
-/*
-	private void sendInt(int msg)
-	{
-		byte[] newMsg = new byte[4];
-		newMsg[0] = (byte)(msg >>> 24);
-		newMsg[1] = (byte)((msg << 8) >> 24);
-		newMsg[2] = (byte)((msg << 16) >> 24);
-		newMsg[3] = (byte)((msg << 24) >> 24);
-		try
-		{
-			out.write(newMsg);
-			out.flush();
-		}
-		catch(IOException e)
-		{
-			System.out.println("Något gick fel när det skulle skickas.");
-		}
-	}
+    // Läser in en lista och returnerar den.
+    private ArrayList<String> receiveListString(int size)
+    {
+        ArrayList<String> toReturn = new ArrayList<String>();
+        for(int i=0; i<size; i++)
+        {
+            toReturn.add(receiveString());
+        }
+        return toReturn;
+    }
+
+    // Public method for connecting to the server.
+    public String connect(String name)
+    {
+        Packet toSend = new Packet((byte)2);
+        toSend.add(protocolVersion);
+        toSend.add(name);
+        send(toSend);
+        return receive();
+    }
+
+    // Method to test sending and receiving lists.
+    public String listTest(ArrayList list)
+    {
+        Packet toSend = new Packet((byte)13);
+        toSend.add(list);
+        send(toSend);
+        return receive();
+    }
+
+    // Method for testing-purposes
+    public String serverTest()
+    {
+        send(new Packet((byte)1));
+        return receive();
+    }
+
+    /*
+       private void sendInt(int msg)
+       {
+       byte[] newMsg = new byte[4];
+       newMsg[0] = (byte)(msg >>> 24);
+       newMsg[1] = (byte)((msg << 8) >> 24);
+       newMsg[2] = (byte)((msg << 16) >> 24);
+       newMsg[3] = (byte)((msg << 24) >> 24);
+       try
+       {
+       out.write(newMsg);
+       out.flush();
+       }
+       catch(IOException e)
+       {
+       System.out.println("Något gick fel när det skulle skickas.");
+       }
+       }
 
 */
-	public static void main(String [] args)
-	{
-		testklient1 k = new testklient1("130.237.238.225", 1234);//130.229.128.72", 1234);
-		/*
-		Packet test = new Packet((byte)2);
-		test.add(0);
-		test.add("Kalle");
-		k.send(test, true);
-		*/
-		System.out.println(k.connect("Kalle"));
-		ArrayList<String> skaMed = new ArrayList<String>();
-		skaMed.add("Hej");
-		skaMed.add("Magnus");
-		System.out.println(k.listTest(skaMed));
-		System.out.println(k.serverTest());
-	}
+    public static void main(String [] args)
+    {
+        testklient1 k = new testklient1("130.237.238.225", 1234);//130.229.128.72", 1234);
+        /*
+           Packet test = new Packet((byte)2);
+           test.add(0);
+           test.add("Kalle");
+           k.send(test, true);
+           */
+        System.out.println(k.connect("Kalle"));
+        ArrayList<String> skaMed = new ArrayList<String>();
+        skaMed.add("Hej");
+        skaMed.add("Magnus");
+        System.out.println(k.listTest(skaMed));
+        System.out.println(k.serverTest());
+    }
 }
--- testserver1.erl
+++ testserver1.erl
@@ -9,221 +9,221 @@
 -define(PROTOCOLVERSION, 0).
 
 init() ->
-	{ok, ListenSocket} = gen_tcp:listen(1234, [binary, {active, false}]),
-	accept(ListenSocket, 0).
+    {ok, ListenSocket} = gen_tcp:listen(1234, [binary, {active, false}]),
+    accept(ListenSocket, 0).
 
 accept(ListenSocket, Count) ->
-	{ok, Socket} = gen_tcp:accept(ListenSocket),
-	io:format("Accepted Connection\n"),
-	spawn(?MODULE, accept, [ListenSocket, Count+1]),
-	try
-		recv(Socket)
-	catch
-		error:X ->
-			case X of
-				{badmatch,{error,closed}} ->
-					ok;
-				_ ->	 
-					io:format("Fångade fel: "), 
-					io:format("~p\n", [X]) 
-			end
-	end.
+    {ok, Socket} = gen_tcp:accept(ListenSocket),
+    io:format("Accepted Connection\n"),
+    spawn(?MODULE, accept, [ListenSocket, Count+1]),
+    try
+        recv(Socket)
+    catch
+        error:X ->
+            case X of
+                {badmatch,{error,closed}} ->
+                    ok;
+                _ ->	 
+                    io:format("Fångade fel: "), 
+                    io:format("~p\n", [X]) 
+            end
+    end.
 
 readHeader(Socket) -> % Tar ut den första byten och tolkar som Headern utifrån protokollet. 
-	{ok, <<Byte?HEADER>>} = gen_tcp:recv(Socket, 1),
-	io:format("Read the following header: "),
-	io:format("~w\n", [Byte]),
-	Byte.
+    {ok, <<Byte?HEADER>>} = gen_tcp:recv(Socket, 1),
+    io:format("Read the following header: "),
+    io:format("~w\n", [Byte]),
+    Byte.
 
 readBoolean(Socket) -> % Tar ut den första byten och tolkar som boolean utifrån protokollet. 
-	{ok, <<Byte?BOOLEAN>>} = gen_tcp:recv(Socket, 1),
-	io:format("Read the following boolean: "),
-	io:format("~w\n", [Byte]),
-	Byte.
+    {ok, <<Byte?BOOLEAN>>} = gen_tcp:recv(Socket, 1),
+    io:format("Read the following boolean: "),
+    io:format("~w\n", [Byte]),
+    Byte.
 
 readInteger(Socket) ->% Tar ut de första 4 bytes och tolkar som Integer utifrån protokollet. 
-	{ok, <<Integer?INTEGER>>} = gen_tcp:recv(Socket, 4),
-	io:format("Read the following integer: "),
-	io:format("~w\n", [Integer]),
-	Integer.
+    {ok, <<Integer?INTEGER>>} = gen_tcp:recv(Socket, 4),
+    io:format("Read the following integer: "),
+    io:format("~w\n", [Integer]),
+    Integer.
 
 readString(Socket) ->% Tar ut bytes tills /0 påträffas, tolkar sedan som String utifrån protokollet
-	readString(Socket, []).
+    readString(Socket, []).
 
 readString(Socket, List) ->
-	{ok, <<Char?CHARACTER>>} = gen_tcp:recv(Socket, 1),
-	case Char of
-		0 ->
-			io:format("Read the following string: "),
-			io:format("~p\n", [lists:reverse(List)]),
-			lists:reverse(List);
-		_ ->
-			readString(Socket, [Char|List])
-	end.
+    {ok, <<Char?CHARACTER>>} = gen_tcp:recv(Socket, 1),
+    case Char of
+        0 ->
+            io:format("Read the following string: "),
+            io:format("~p\n", [lists:reverse(List)]),
+            lists:reverse(List);
+        _ ->
+            readString(Socket, [Char|List])
+    end.
 
 readList(Socket) ->
-	NumberOfElements = readInteger(Socket),
-	ElemType = readString(Socket),
-	readList(Socket, NumberOfElements, ElemType, []).
+    NumberOfElements = readInteger(Socket),
+    ElemType = readString(Socket),
+    readList(Socket, NumberOfElements, ElemType, []).
 
 readList(_, 0, _, List) ->
-	lists:reverse(List);
+    lists:reverse(List);
 
 readList(Socket, NumberOfElements, "Integer", List) ->
-	Element = readInteger(Socket),
-	readList(Socket, NumberOfElements-1, "Integer", [Element|List]);
+    Element = readInteger(Socket),
+    readList(Socket, NumberOfElements-1, "Integer", [Element|List]);
 
 readList(Socket, NumberOfElements, "Boolean", List) ->
-	Element = readBoolean(Socket),
-	readList(Socket, NumberOfElements-1, "Boolean", [Element|List]);
+    Element = readBoolean(Socket),
+    readList(Socket, NumberOfElements-1, "Boolean", [Element|List]);
 
 readList(Socket, NumberOfElements, "String", List) ->
-	Element = readString(Socket),
-	readList(Socket, NumberOfElements-1, "String", [Element|List]).
+    Element = readString(Socket),
+    readList(Socket, NumberOfElements-1, "String", [Element|List]).
 
 readPerhaps(Socket) ->
-	Perhaps = readBoolean(Socket),
-	case Perhaps of
-		1 ->
-			ElemType = readString(Socket),
-			readPerhaps(Socket, ElemType);
-		_ ->
-			none
-	end.
+    Perhaps = readBoolean(Socket),
+    case Perhaps of
+        1 ->
+            ElemType = readString(Socket),
+            readPerhaps(Socket, ElemType);
+        _ ->
+            none
+    end.
 
 readPerhaps(Socket, "Integer") ->
-	readInteger(Socket);
+    readInteger(Socket);
 
 readPerhaps(Socket, "String") ->
-	readString(Socket);
+    readString(Socket);
 
 readPerhaps(Socket, "Boolean") ->
-	readBoolean(Socket).
+    readBoolean(Socket).
 
 sendHeader(Socket, Header) ->
-	gen_tcp:send(Socket, <<Header?HEADER>>),
-	io:format("SendHeader sent: "),
-	io:format("~p\n", [Header]).
+    gen_tcp:send(Socket, <<Header?HEADER>>),
+    io:format("SendHeader sent: "),
+    io:format("~p\n", [Header]).
 
 sendString(Socket, List) ->
-	NewList = List ++ "\0",
-	gen_tcp:send(Socket, NewList),
-	io:format("SendString sent: "),
-	io:format("~p\n", [NewList]).
+    NewList = List ++ "\0",
+    gen_tcp:send(Socket, NewList),
+    io:format("SendString sent: "),
+    io:format("~p\n", [NewList]).
 
 sendInteger(Socket, Int) ->
-	gen_tcp:send(Socket, <<Int?INTEGER>>),
-	io:format("SendInteger sent: "),
-	io:format("~p\n", [Int]).
+    gen_tcp:send(Socket, <<Int?INTEGER>>),
+    io:format("SendInteger sent: "),
+    io:format("~p\n", [Int]).
 
 sendBoolean(Socket, Bool) ->
-	gen_tcp:send(Socket, <<Bool?BOOLEAN>>),
-	io:format("SendBoolean sent: "),
-	io:format("~p\n", [Bool]).
+    gen_tcp:send(Socket, <<Bool?BOOLEAN>>),
+    io:format("SendBoolean sent: "),
+    io:format("~p\n", [Bool]).
 
 sendList(Socket, ElemType, List) ->
-	NumberOfElements = length(List),
-	NewType = ElemType ++ "\0",
-	case ElemType of 
-		"String" ->
-			NewList = [X ++ "\0"|| X <- List], % Lägger till nullterminering på alla strängar i listan
-			Packet = [<<NumberOfElements?INTEGER>>, NewType, NewList];
-	       	"Integer" ->
-			IntList = [<<X?INTEGER>> || X <- List], % Tolkar alla intar i listan som ?INTEGER
-		       	Packet = [<<NumberOfElements?INTEGER>>, NewType, IntList];
-		Other ->
-			io:format("Invalid ElemType in function sendList!"),
-			Packet = "Error!\0"
-	end,	       
-	
-	gen_tcp:send(Socket, Packet),
-	io:format("SendList sent: "),
-	io:format("~p\n", [List]).
+    NumberOfElements = length(List),
+    NewType = ElemType ++ "\0",
+    case ElemType of 
+        "String" ->
+            NewList = [X ++ "\0"|| X <- List], % Lägger till nullterminering på alla strängar i listan
+            Packet = [<<NumberOfElements?INTEGER>>, NewType, NewList];
+        "Integer" ->
+            IntList = [<<X?INTEGER>> || X <- List], % Tolkar alla intar i listan som ?INTEGER
+            Packet = [<<NumberOfElements?INTEGER>>, NewType, IntList];
+        Other ->
+            io:format("Invalid ElemType in function sendList!"),
+            Packet = "Error!\0"
+    end,	       
+
+    gen_tcp:send(Socket, Packet),
+    io:format("SendList sent: "),
+    io:format("~p\n", [List]).
 
 sendPerhaps(Socket, false) ->
-	sendInteger(Socket, 0).
+    sendInteger(Socket, 0).
 
 sendPerhaps(Socket, true, Type, Elem) ->
-	sendInteger(Socket, 1),
-	sendString(Socket, Type),
-	case Type of
-		"Integer" ->
-			sendInteger(Socket, Elem);
-		"String" ->
-			sendString(Socket, Elem);
-		"Boolean" ->
-			sendBoolean(Socket, Elem)
-	end.
+    sendInteger(Socket, 1),
+    sendString(Socket, Type),
+    case Type of
+        "Integer" ->
+            sendInteger(Socket, Elem);
+        "String" ->
+            sendString(Socket, Elem);
+        "Boolean" ->
+            sendBoolean(Socket, Elem)
+    end.
 
 sendFailPacket(FailureID, ReqHeader, Socket) -> % 1:Number to identify failure. 2:Head of failed package. NEGATIVT FAILUREID FUNGERAR EJ MED LIST2BINARY 
-	io:format("Created the following failmsg: "),
-	FailText = getFailMsg(FailureID), % Text that describes the failure
+    io:format("Created the following failmsg: "),
+    FailText = getFailMsg(FailureID), % Text that describes the failure
 
-	sendHeader(Socket, 0),
-	sendInteger(Socket, FailureID),
-	sendHeader(Socket, ReqHeader),
-	sendString(Socket, FailText).
+    sendHeader(Socket, 0),
+    sendInteger(Socket, FailureID),
+    sendHeader(Socket, ReqHeader),
+    sendString(Socket, FailText).
 
 getFailMsg(FailureType) ->
-	case FailureType of
-		-1 ->
-			Msg = "Invalid state";
-		0 ->
-			Msg = "Invalid protocol version";
-		1 ->
-			Msg = "Name already exists";
-		2 ->
-			Msg = "Game does not exist";
-		3 ->
-			Msg = "Game is locked";
-		4 ->
-			Msg = "Permission denied";
-		5 ->
-			Msg = "Occupied tile";
-		6 ->
-			Msg = "No movement left";
-		7 ->
-			Msg = "Invalid tile";
-		8 ->
-			Msg = "Build in process";
-		9 ->
-			Msg = "Building already exists";
-		10 ->
-			Msg = "Friendly fire is off";
-		11 ->
-			Msg = "Out of range";
-		12 ->
-			Msg = "Building does not exist"
-	end,
-	Msg.
+    case FailureType of
+        -1 ->
+            Msg = "Invalid state";
+        0 ->
+            Msg = "Invalid protocol version";
+        1 ->
+            Msg = "Name already exists";
+        2 ->
+            Msg = "Game does not exist";
+        3 ->
+            Msg = "Game is locked";
+        4 ->
+            Msg = "Permission denied";
+        5 ->
+            Msg = "Occupied tile";
+        6 ->
+            Msg = "No movement left";
+        7 ->
+            Msg = "Invalid tile";
+        8 ->
+            Msg = "Build in process";
+        9 ->
+            Msg = "Building already exists";
+        10 ->
+            Msg = "Friendly fire is off";
+        11 ->
+            Msg = "Out of range";
+        12 ->
+            Msg = "Building does not exist"
+    end,
+    Msg.
 
 
 recv(Socket) ->
-	Header = readHeader(Socket),
-	case Header of 
-		 1 ->
-			 
-			 sendHeader(Socket, 1),
-			 sendInteger(Socket, 4),
-			 sendString(Socket, "hejsan");
-		 13 ->  
-			TheList = readList(Socket),
-			io:format("Listan: "),
-			io:format("~p\n", [TheList]),
-			sendHeader(Socket, 6),
-			%sendList(Socket, "Integer", [1, 2, 3, 4, 5, 6, 8]);
-			sendList(Socket, "String", ["Maggan", "Fredde", "Simon", "De var ett vackert par"]);
-		2 -> % Hello World
-			ProtocolVersion = readInteger(Socket),
+    Header = readHeader(Socket),
+    case Header of 
+        1 ->
 
-			case ProtocolVersion =:= ?PROTOCOLVERSION of
-				true ->
-					PlayerName = readString(Socket),
-					Response = list_to_binary([<<3?HEADER>>]),
-					gen_tcp:send(Socket, Response);
-				false ->
-					readString(Socket),
-					sendFailPacket(0, Header, Socket)
-			end
-	end,
-	recv(Socket).
+            sendHeader(Socket, 1),
+            sendInteger(Socket, 4),
+            sendString(Socket, "hejsan");
+        13 ->  
+            TheList = readList(Socket),
+            io:format("Listan: "),
+            io:format("~p\n", [TheList]),
+            sendHeader(Socket, 6),
+            %sendList(Socket, "Integer", [1, 2, 3, 4, 5, 6, 8]);
+            sendList(Socket, "String", ["Maggan", "Fredde", "Simon", "De var ett vackert par"]);
+        2 -> % Hello World
+            ProtocolVersion = readInteger(Socket),
+
+            case ProtocolVersion =:= ?PROTOCOLVERSION of
+                true ->
+                    PlayerName = readString(Socket),
+                    Response = list_to_binary([<<3?HEADER>>]),
+                    gen_tcp:send(Socket, Response);
+                false ->
+                    readString(Socket),
+                    sendFailPacket(0, Header, Socket)
+            end
+    end,
+    recv(Socket).
