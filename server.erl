4c1c99a8437070e6c97cf525d45271a631cd9c2d
Mariana Simion Rönngårdh <marianaronngardh@gmail.com>
2011-04-28 11:59:29 +0200

    i servermodulen: gör om alla Players till lowerCase å kollar om de redan finns

--- server.erl
+++ server.erl
@@ -20,11 +20,28 @@ handle_call(list_players, _From, {Games, Players}) ->
     {reply, [Player#player.name || Player <- Players], {Games,Players}};
 handle_call(list_games, _From, {Games, Players}) ->
     {reply, [Game#game.name || Game <- Games], {Games,Players}};
-handle_call({add_player, Player}, _From, {Games, Players}) ->
-    io:format("Added player ~w~n", [Player]),
-    {reply, ok, {Games, [Player|Players]}}.
+handle_call({add_player, Player}, _From, {Games, Players}) -> %Check if playerId already exists
+    
+    ListOfPlayerNames = [Player#player.name || Player <- Players],
+    Pfun = fun(OldPlayer) ->
+	      Player == OldPlayer
+    end,
+	    		
+    any(Pfun, ListOfPlayerNames) ->
+       true ->
+	    io:format("Playername already exists ~w~n",[Player],
+    	    {reply, false, {Games, Players}};
+	 false ->
+	    %Lägg till i listan	   
+    	    io:format("Added player ~w~n", [Player]),
+    	    {reply, true, {Games, [Player|Players]}}.
+
 
 %Server calls and casts
-add_player(Player) -> gen_server:call(?MODULE, {add_player, Player}).
+add_player(Player) ->  
+    PlayerName = string:to_lower(Player),
+    gen_server:call(?MODULE, {add_player, PlayerName}).
+	  	  
+
 list_players() -> gen_server:call(?MODULE, list_players).
 list_games() -> gen_server:call(?MODULE, list_games).
