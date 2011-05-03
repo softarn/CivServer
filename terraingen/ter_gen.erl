28feebd266853a39ba1171e0ed68868f0b50c183
Arian Jafari <aria-jaf@dsv.su.se>
2011-05-03 15:15:34 +0200

    Map matrix now consists of strings instead of integers (e g 'Plains, Sea, Sea' instead of '3, 2, 2')

--- terraingen/ter_gen.erl
+++ terraingen/ter_gen.erl
@@ -1,19 +1,21 @@
 %	Random terrain generator
-% 	Authors: Arian Jafari & Rasmus Ivö
+% 	Authors: 	Arian Jafari & Rasmus Ivö
+%	aria-jaf@dsv.su.se
+%	rasm-ivo@dsv.su.se
 % =============================
-% The method "generate" takes width and height
-% and returns a matrix represented of Lists
-% lists.
+% The method generate/2 takes two parameters; width and height, 
+% and returns a matrix (e g lists containing lists).
 % 
-% The matrix contains integers ranging between 1 and 12,
-% each representing a type of terrain. Preferrably we follow
-% the list presented in the "Specifications 2.0" document.
+% Originally, the matrix contains integers ranging between 1 and 12,
+% each representing a type of terrain. The matrix is then manipulated,
+% with each element being replaced by a string with the name of the
+% terrain type.
 %
 % A lot of the numbers are easily tweaked. By increasing/decreasing
 % the WATER_FACTOR constant, you can change the weighted ratio of
 % water to land. Change the LANDTYPE_FACTOR constant in order to 
 % increase/decrease the influence neighbouring terrain types has when
-% determining a position's terraint ype.
+% determining a position's terrain type.
 %
 % Ocean tiles are just generated when all neighbours is water-tiles.
 % Note: the generate_ocean function removes islands (lone land tiles in water)
@@ -23,11 +25,12 @@
 
 -module(ter_gen).
 -export([generate/2]).
--define(WATER_FACTOR, 5).
--define(LANDTYPE_FACTOR, 5).
+-define(WATER_FACTOR, 6).		% Decrease for more water, and vice versa.
+-define(LANDTYPE_FACTOR, 5).	% Decrease for higher terrain homogeneity, and vice versa.
 
-% Returns a matrix (a list containing lists) representing a randomly
-% generated terrain map, with the given width and height.
+%% Returns a matrix (a list containing lists) representing a randomly
+%% generated terrain map, with the given width and height.
+%% This is the only method you need to call.
 generate(Width,Height) when is_number(Width), is_number(Height)->
 	TheMap = generate_ocean(create_map(Width, Height)),
 	% Water = check_map(Width, Height, TheMap, Width, Height, 0),
@@ -38,15 +41,15 @@ generate(Width,Height) when is_number(Width), is_number(Height)->
 	%		ok
 	% end
 	% generate_ocean(TheMap),	
-	[tuple_to_list(Column)|| Column <- tuple_to_list(TheMap)].
-
+	StringMap = stringify(TheMap, Width, Height, Width, Height),
+	[tuple_to_list(Column)|| Column <- tuple_to_list(StringMap)].
 
 create_map(Width,Height)->
 	Matrix = erlang:make_tuple(Width,erlang:make_tuple(Height,1)),
 	generate_tiles(Width, Height, Matrix, Width, Height).
 
-% Iterates through the matrix, procedurally 
-% choosing the terrain type for each position.
+%% Iterates through the matrix, procedurally 
+%% choosing the terrain type for each position.
 generate_tiles(0,_, Matrix,_, _) ->
 	Matrix;
 generate_tiles(Row, 1, Matrix, Width, Height) ->
@@ -56,20 +59,16 @@ generate_tiles(Row, Column, Matrix, Width, Height) ->
 	NewMatrix = choose_tile(Row, Column, Matrix),
 	generate_tiles(Row, Column-1, NewMatrix, Width, Height).
 
-% Sets the integer of a given position in the matrix.
+%% Sets the integer of a given position in the matrix.
 change_terrain(Row,Column,Terrain,Matrix)->
 	setelement(Row,Matrix,setelement(Column,element(Row,Matrix),Terrain)).	
 
-% Returns the integer of a given position in the matrix.
+%% Returns the integer of a given position in the matrix.
 get_terrain(Row,Column,Matrix)->
 	element(Column,element(Row,Matrix)).
 
-% Coordinates for finding a certain tile's neighbours.
-build_directions()->
-	[{1,0},{-1,0},{0,1},{0,-1},{1,1},{-1,-1}].
-
-% Returns a list containing the coordinates
-% of a given tile's neighbours.
+%% Returns a list containing the coordinates
+%% of a given tile's neighbours.
 get_neighbours(_,_,List,[],_) ->
 	List;
 get_neighbours(Row,Column,_,_,Matrix) when Row<1; Row>tuple_size(Matrix); Column<1;Column>tuple_size(element(Row,Matrix))->
@@ -86,8 +85,8 @@ get_neighbours(Row,Column,List,[{Y,X}|Tail],Matrix) ->
 			end
 	end.
 
-% Takes a list of integers and counts the number of times a certain
-% integer (representing a terrain type) appears in the given list.
+%% Takes a list of integers and counts the number of times a certain
+%% integer (representing a terrain type) appears in the given list.
 count_terrain([],_,_,Acc)->
 	Acc;
 count_terrain([{Y,X}|Tail],Matrix,Terrain,Acc)->
@@ -96,7 +95,8 @@ count_terrain([{Y,X}|Tail],Matrix,Terrain,Acc)->
 		_->	count_terrain(Tail,Matrix,Terrain,Acc)
 	end. 	
 
-% Determines the type of land a certain tile will have.
+%% Determines the type of land a certain tile will have through
+%% a weighted random number, determined by adjusting to the adjacent terrain tiles.
 det_type(Row,Column,Matrix,_,2,Terrain,_)->
 		change_terrain(Row, Column, Terrain, Matrix);
 det_type(Row,Column,Matrix,Neigh,Count,Terrain,Max)->
@@ -106,19 +106,12 @@ det_type(Row,Column,Matrix,Neigh,Count,Terrain,Max)->
 			false->det_type(Row,Column,Matrix,Neigh,Count-1,Terrain,Max)
 		end.
 
-type_of_water(Row,Column,Matrix, [])->
-	change_terrain(Row, Column, 2, Matrix);	
-type_of_water(Row,Column,Matrix, [{Y,X}|Tail])->
-	case get_terrain(Y,X,Matrix) < 3 of
-			true -> type_of_water(Row,Column,Matrix,Tail);
-			false -> Matrix	end.
-
-% Method for choosing the terrain type for a given position in the matrix.
-% Gathers the terrain type of the tile's neighbours, and then makes
-% a random (yet weighted) decision based upon the neighbouring terrain types.
-% Returns the updated matrix with the performed changes.
+%% Method for choosing the terrain type for a given position in the matrix.
+%% Gathers the terrain type of the tile's neighbours, and then makes
+%% a random (yet weighted) decision based upon the neighbouring terrain types.
+%% Returns the updated matrix with the performed changes.
 choose_tile(Row,Column,Matrix)->
-	Neigh = get_neighbours(Row,Column,[],build_directions(),Matrix),	
+	Neigh = get_neighbours(Row,Column,[],neighbour_coords(),Matrix),	
 	Water = count_terrain(Neigh,Matrix,1,0),
 	Weight = Water - (length(Neigh) - Water),
 	WaterOrLand = (random:uniform(?WATER_FACTOR) - 1) - Weight,
@@ -129,18 +122,26 @@ choose_tile(Row,Column,Matrix)->
 			Matrix
 	end.
 
-% Goes through the matrix and changes Sea (1) tiles completely surrounded
-% by water into Ocean (2) tiles. Also eliminates one tile large islands,
-% e.g. land tiles surrounded by 6 water tiles.
+%% Goes through the matrix and changes Sea (1) tiles completely surrounded
+%% by water into Ocean (2) tiles. Also eliminates one tile large islands,
+%% e.g. land tiles surrounded by 6 water tiles.
 generate_ocean(Matrix)->
 	generate_ocean(Matrix,tuple_size(Matrix),tuple_size(element(1,Matrix))).
 generate_ocean(Matrix,1,1)->
-	type_of_water(1,1,Matrix,get_neighbours(1,1,[],build_directions(),Matrix));	
+	type_of_water(1,1,Matrix,get_neighbours(1,1,[],neighbour_coords(),Matrix));	
 generate_ocean(Matrix,Row,1)->
-	generate_ocean(type_of_water(Row,1,Matrix,get_neighbours(1,1,[],build_directions(),Matrix)),Row-1,tuple_size(element(Row-1,Matrix)));
+	generate_ocean(type_of_water(Row,1,Matrix,get_neighbours(1,1,[],neighbour_coords(),Matrix)),Row-1,tuple_size(element(Row-1,Matrix)));
 generate_ocean(Matrix,Row,Column)->
-	generate_ocean(type_of_water(Row,Column,Matrix,get_neighbours(Row,Column,[],build_directions(),Matrix)),Row,Column-1).
+	generate_ocean(type_of_water(Row,Column,Matrix,get_neighbours(Row,Column,[],neighbour_coords(),Matrix)),Row,Column-1).
 
+type_of_water(Row,Column,Matrix, [])->
+	change_terrain(Row, Column, 2, Matrix);	
+type_of_water(Row,Column,Matrix, [{Y,X}|Tail])->
+	case get_terrain(Y,X,Matrix) < 3 of
+			true -> type_of_water(Row,Column,Matrix,Tail);
+			false -> Matrix	end.
+
+%% -----STUB-----
 %% Counts the number of water tiles the matrix contains.
 %% To be used for checking whether a map contains a sufficient
 %% ratio of water:land or not, possibly re-generating the map
@@ -157,3 +158,26 @@ generate_ocean(Matrix,Row,Column)->
 %			NewAcc = Acc
 %	end,
 %	check_map(Row, Column-1, Matrix, Width, Height, NewAcc).
+
+%% Converts the integer matrix to a String matrix containing
+%% names of the different terrain types.
+stringify(Matrix, 1, 0, _, _) ->
+	Matrix;
+stringify(Matrix, Row, 0, Width, Height) ->
+	stringify(Matrix, Row-1, Height, Width, Height);
+stringify(Matrix, Row, Column, Width, Height) ->
+	TerInt = get_terrain(Row, Column, Matrix),
+	TerStr = element(TerInt, terrain_names()),
+	NewMatrix = change_terrain(Row, Column, TerStr, Matrix),
+	stringify(NewMatrix, Row, Column-1, Width, Height).
+	
+	
+%% Coordinates for finding a certain tile's neighbours.
+neighbour_coords()->
+	[{1,0},{-1,0},{0,1},{0,-1},{1,1},{-1,-1}].
+	
+%% Used as a map-like data structure for swapping out integers to terrain names.
+terrain_names() ->
+	{"Sea", "Ocean", "Plains", "Grassland", "Marsh", "Desert",
+	"Tundra", "Rain Forrest", "Conifer Forrest", "Broadleaf Forrest",
+	"Hills", "Mountains"}.
