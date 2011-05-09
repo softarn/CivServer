169ca8e4bd2445f8439723ba1b87ce923ccdbf98
Fredrik Norberg <fnorbe@dsv.su.se>
2011-05-09 18:49:19 +0200

    Har lagt till funktionalitet i testklienten för att ta emot startunits

--- TestKlient.java
+++ TestKlient.java
@@ -4,6 +4,7 @@ class TestKlient{
 	String name, ip = "130.229.157.246";
 	Proxy p;
 	int port;
+	boolean loop = true;
 	Scanner sc = new Scanner(System.in);
 	Scanner scan = new Scanner(System.in);
 
@@ -25,10 +26,12 @@ class TestKlient{
 		p = new Proxy(ip, port, new MyPackLyss());
 
 		Result returned;
+		
+		int what;
 
-		while(true){
+		while(loop){
 			System.out.println("What you wanna do?\n1: connect\n2: list games\n3: host game\n4: start game\n5: join game\n0:quit");
-			int what = sc.nextInt();
+			 what = sc.nextInt();
 
 			switch(what){
 				case 0:
@@ -76,7 +79,8 @@ class TestKlient{
 					catch(FailedException fe){
 						System.out.println(fe);
 					}
-					break;
+					loop = false;
+					return;
 				case 5:
 					try{
 						p.joinGame(scan.next());
@@ -87,6 +91,35 @@ class TestKlient{
 					break;
 			}
 		}
+			
+			
+
+	}
+
+
+	public void inGame(){
+		int what;
+		Result returned;
+
+		 while(true){
+				System.out.println("What you wanna do?\n1: list unitst\n2: move unit\n3: end turn\n0:quit");
+			 what = sc.nextInt();
+			    switch(what){
+				   case 0:
+					 System.exit(0);
+					
+				    case 1:
+				    	try{
+					        returned = p.connect(name);
+					        System.out.println(returned.getOk());  
+					}
+					catch(FailedException fe){
+						System.out.println(fe);
+					}
+					break;
+			    }
+			 }
+
 	}
 
 	public class Unit{
@@ -126,6 +159,30 @@ class TestKlient{
 
 		public void gameStarted(Result res){
 			System.out.println(res.getMap());
+			loop = false;
+			int numberOfTiles = res.getNumberTiles();
+			for(int i=0; i<numberOfTiles; i++){
+				int tileXValue = res.getTileX(i);
+				int tileYValue = res.getTileY(i);
+			if(res.existUnit(i)){
+				String theOwner = res.getUnitOwner(i);
+				String theType = res.getUnitType(i);
+				System.out.println(theOwner + " " + theType + " x:" + tileXValue+ "y: "+ tileYValue);
+				int manPowerLeft = res.getUnitManPower(i);
+			}
+			if(res.existCity(i)){
+				String theOwner = res.getCityOwner(i);
+				String theName = res.getCityName(i);
+				List<String> buildings = res.getCityBuildings(i);
+				int amountCityUnits = res.getAmountCityUnits(i);
+				for(int j=0; j<amountCityUnits; j++){
+					String cityUnitOwner = res.getCityUnitOwner(i, j);
+					String cityUnitType = res.getCityUnitType(i, j);
+					int cityUnitManpower = res.getCityUnitManPower(i, j);
+				}
+			}
+		String improvement = res.getImprovement(i);
+		}
 		}
 
 		public void chatMessageReceived(Result res){
