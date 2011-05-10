4f8c99d69501b2ce18143c89e2c9d266a453b096
Magnus Eriksson <Chylis88gmail.com>
2011-05-10 16:56:07 +0200

    added erlang: before min/2 and max/2 functions

--- combat.erl
+++ combat.erl
@@ -5,7 +5,7 @@
 get_range({X1,Y1},{X2,Y2})->
 	Z1 = X1 - Y1,		
 	Z2 = X2 - Y2,		
-	max(max(dif(X1,X2),dif(Y1,Y2)),dif(Z1,Z2)).
+	erlang:max(erlang:max(dif(X1,X2),dif(Y1,Y2)),dif(Z1,Z2)).
 dif(X1,X2)->
-	max(X1,X2) - min (X1,X2).
+	erlang:max(X1,X2) - erlang:min (X1,X2).
 	
