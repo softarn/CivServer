46012fbb4275625a9e206f15d3d1c67a67304fcb
Arian Jafari <aria-jaf@dsv.su.se>
2011-05-16 15:37:01 +0200

    test

--- combat_eval.erl
+++ combat_eval.erl
@@ -12,30 +12,35 @@
 %%
 %% Terrain bonuses (e g attack/defense combat modifiers)
 %% are not yet implemented, but it will be implemented in
-%% the next version.
+%% the next version..
 
 -module(combat_eval).
--export([combat/4]).
+-export([combat/6]).
 
-combat(Attacker, AtkMP, Defender, DefMp) ->
-    AttStr = string:to_lower(Attacker),
-    DefStr = string:to_lower(Defender),
+combat(Attacker, AtkMP, AtkTerr, Defender, DefMp, DefTerr) ->
+	AttStr = string:to_lower(Attacker),
+	AtkTerrStr = string:to_lower(AtkTerr),
+	DefTerrStr = string:to_lower(DefTerr),
+	DefStr = string:to_lower(Defender),
     case element(6, unit_attr:get_attr(list_to_atom(AttStr))) of
 		assault ->
-		    assault_combat(list_to_atom(AttStr), AtkMP, list_to_atom(DefStr), DefMp);
+		    assault_combat(list_to_atom(AttStr), AtkMP, AtkTerrStr, list_to_atom(DefStr), DefMp, DefTerrStr);
 		range ->
-			ranged_combat(list_to_atom(AttStr), AtkMP, DefMp);
+			ranged_combat(list_to_atom(AttStr), AtkMP, AtkTerrStr, DefMp);
 		bombardment ->
-			bombard_combat(list_to_atom(AttStr), AtkMP, DefMp)
+			bombard_combat(list_to_atom(AttStr), AtkMP, AtkTerrStr, DefMp)
 	end.
-	
-assault_combat(Atk, AtkMP, Def, DefMP) ->
+
+assault_combat(Atk, AtkMP, AtkTerr, Def, DefMP, DefTerr) ->
+	random:seed(now()),
 	Charges = random:uniform(12) + random:uniform(12),
 	AtkAttrs = unit_attr:get_attr(Atk),
 	DefAttrs = unit_attr:get_attr(Def),
+	AtkBonus = 1 + element(1, terr_attr:get_attr(AtkTerr)),
+	DefBonus = 1 + element(2, terr_attr:get_attr(DefTerr)),
 	AP = element(2, AtkAttrs),
 	% Add attack bonus from terrain
-	% NewAP = AP * TerrAtk,
+	NewAP = round(AP * AtkBonus),
 	case Def of
 		pikeman ->
 			case element(7, unit_attr:get_attr(Atk)) of
@@ -47,10 +52,10 @@ assault_combat(Atk, AtkMP, Def, DefMP) ->
 		_ ->
 			DP = (element(3, DefAttrs))
 	end,
-	%DP = element(3, DefAttrs),
-	% Add defense bonus
-	% NewDP = DP * TerrDef,
-	assault_charge(AP, AtkMP, DP, DefMP, Charges).
+	% DP = element(3, DefAttrs),
+	% Add defense bonus from terrain
+	NewDP = round(DP * DefBonus),
+	assault_charge(NewAP, AtkMP, NewDP, DefMP, Charges).
 
 assault_charge(_, AtkMP, _, DefMP, 0) ->
 	{AtkMP, DefMP};
@@ -65,13 +70,14 @@ assault_charge(AP, AtkMP, DP, DefMP, Charges) ->
 	NewDefMP = DefMP - DAU,
 	assault_charge(AP, NewAtkMP, DP, NewDefMP, Charges-1).
 
-ranged_combat(Attacker, AtkMP, DefMP) ->
+ranged_combat(Attacker, AtkMP, AtkTerr, DefMP) ->
+	random:seed(now()),
 	Charges = random:uniform(3) + random:uniform(3),
 	AtkAttrs = unit_attr:get_attr(Attacker),
 	AP = element(2, AtkAttrs),
-	% Add attack bonus from terrain
-	% NewAP = AP * TerrAtk,
-	ranged_charge(AP, AtkMP, DefMP, Charges).
+	% Add attack bonus from terrain.
+	NewAP = round(AP * (1 + element(1, terr_attr:get_attr(AtkTerr)))),
+	ranged_charge(NewAP, AtkMP, DefMP, Charges).
 	
 ranged_charge(_, AtkMP, DefMP, 0) ->
 	{AtkMP, DefMP};
@@ -83,12 +89,13 @@ ranged_charge(AP, AtkMP, DefMP, Charges) ->
 	NewDefMP = DefMP - DAU,
 	ranged_charge(AP, AtkMP, NewDefMP, Charges-1).
 
-bombard_combat(Attacker, AtkMP, DefMP) ->
+bombard_combat(Attacker, AtkMP, AtkTerr, DefMP) ->
+	random:seed(now()),
 	AtkAttrs = unit_attr:get_attr(Attacker),
 	AP = element(2, AtkAttrs),
 	% Add attack bonus from terrain
-	% NewAP = AP * TerrAtk,
-	AUDP = round((AP * AtkMP) / 100),
+	NewAP = round(AP * (1 + element(1, terr_attr:get_attr(AtkTerr)))),
+	AUDP = round((NewAP * AtkMP) / 100),
 	DAU = (random:uniform(AUDP + 1) - 1) + (random:uniform(AUDP + 1) - 1),
 	NewDefMP = DefMP - DAU,
 	{AtkMP, NewDefMP}.
